3fab1fb27c2ff76c143106990f29b486e78811cd
Flora Fischbacher <227881669+FloraFischbacher@users.noreply.github.com>
2025-10-18 22:20:07 -0500

    Finish sketch of dependency graph

--- src/main/java/dev/floraf/loader/data/DependencyGraph.java
+++ src/main/java/dev/floraf/loader/data/DependencyGraph.java
@@ -1,6 +1,7 @@
 package dev.floraf.loader.data;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 /**
  * An implementation of a directed acyclic graph that serves as a means of
@@ -9,23 +10,22 @@ import java.util.*;
  * that the graph is, in fact, a DAG.
  */
 public class DependencyGraph {
-    /**
-     *
-     */
-    private int elements;
-    /**
-     *
-     */
-    private boolean valid = false;
+    private int current = -1;
+
+    private ArrayList<Vertex> vertices = new ArrayList<>();
 
     /**
      *
      */
-    private ArrayList<Optional<HashSet<Integer>>> incoming;
+    private HashMap<Integer, HashSet<Integer>> incoming = new HashMap<>();
     /**
      *
      */
-    private ArrayList<Optional<HashSet<Integer>>> outgoing;
+    private HashMap<Integer, HashSet<Integer>> outgoing = new HashMap<>();
+    /**
+     * 
+     */
+    private HashMap<MashupPatch, Vertex> byPatchId = new HashMap<>();
 
     /**
      * A "node" on the graph, or (more accurately to this context) a single
@@ -52,27 +52,55 @@ public class DependencyGraph {
             return Objects.hashCode(id);
         }
 
-        public Vertex(MashupPatch data) {}
+        public Vertex(MashupPatch data) {
+            current += 1;
+
+            this.id = current;
+            this.data = data;
+
+            incoming.put(current, new HashSet<>());
+            outgoing.put(current, new HashSet<>());
+            byPatchId.put(data, this);
+        }
     }
 
     /**
      *
      */
     public class Edge {
-        int from;
-        int to;
+        int source;
+        int target;
         DependencyType type;
 
-        public Edge(int from, int to, DependencyType type) {}
+        public Edge(int source, int target, DependencyType type) {
+            // This shouldn't ever happen, but I'm not taking any chances.
+            if (!incoming.containsKey(source) || !incoming.containsKey(target)) {
+                throw new IllegalArgumentException();
+            }
+
+            if (source == target) {
+                throw new IllegalArgumentException(
+                    "A patch can't depend on itself!");
+            }
+
+            outgoing.get(source).add(target);
+            incoming.get(target).add(source);
+        }
     }
 
     /**
-     * An implementation of dependency validation using Tarjan's strongly-
-     * connected-components algorithm. A majority of the work is actually
-     * performed by the depth-first search defined in
-     * {@link DependencyGraph#dfs()}.
+     * Traverses through the graph's vertices to find a list of
+     * @return
+     */
+    public boolean analyze() {
+        return true;
+    }
+
+    /**
+     * An implementation of Tarjan's strongly- connected-components algorithm.
+     * Part two of {@link DependencyGraph#resolve()}.
      *
-     * @see DependencyGraph#dfs()
+     * @see DependencyGraph#resolve()
      * @return A reverse-topological-sorted list of all vertices (i.e. the load
      *         order of all patches in the dependency graph), or a list of sets
      *         of vertices that form dependency cycles.
@@ -81,20 +109,58 @@ public class DependencyGraph {
      *          pattern-match the result.
      */
     public Either<List<Vertex>, List<Set<Vertex>>> validate() {
-        List<Vertex> exampleLeft = new ArrayList<>();
-        List<Set<Vertex>> exampleRight = new ArrayList<>();
+        // By defining the method here, this allows us to keep the DFS-
+        // specific variables contained within the outer method's scope.
+        class DFS {
+            int               dfsIndex = 0;
+            Stack<Vertex>     dfsStack = new Stack<>();
+            List<Set<Vertex>> cycles   = new ArrayList<>();
+            List<Vertex>      order    = new ArrayList<>();
 
-        exampleRight.add(new HashSet<>());
-        exampleRight.add(new HashSet<>());
+            public void search(Vertex vertex) {
+                vertex.index = dfsIndex;
+                vertex.lowlink = dfsIndex;
 
-        if (valid) return new Left<>(exampleLeft);
-        else return new Right<>(exampleRight);
-    }
+                dfsIndex += 1;
+                dfsStack.push(vertex);
+                vertex.onStack = true;
 
-    /**
-     * Implementation of a depth-first search for the dependency graph.
-     */
-    public void dfs() {
+                for (int edge : outgoing.get(vertex.id)) {
+                    Vertex child = vertices.get(edge);
 
+                    if (child.index == -1) {
+                        search(child);
+                        vertex.lowlink = Math.min(vertex.lowlink, child.lowlink);
+                    } else if (child.onStack) {
+                        vertex.lowlink = Math.min(vertex.lowlink, child.index);
+                    }
+                }
+
+                if (vertex.lowlink == vertex.index) {
+                    ArrayList<Vertex> component = new ArrayList<>();
+
+                    Vertex child;
+                    do {
+                        child = dfsStack.pop();
+                        child.onStack = false;
+                        component.add(child);
+                    } while (child.id != vertex.id);
+
+                    if (component.size() > 1) cycles.add(new HashSet<>(component));
+                    else order.add(component.getFirst());
+                }
+            }
+        }
+
+        DFS dfs = new DFS();
+
+        for (Vertex vertex : vertices) {
+            if (vertex.index == -1) {
+                dfs.search(vertex);
+            }
+        }
+
+        if (dfs.cycles.size() > 0) return new Right<>(dfs.cycles);
+        else return new Left<>(dfs.order);
     }
 }
