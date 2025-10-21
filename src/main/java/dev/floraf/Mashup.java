db2491e1f0c3ff2a194d60dc1906a14fceb0bd1e
Flora Fischbacher <227881669+FloraFischbacher@users.noreply.github.com>
2025-10-20 22:46:23 -0500

    Implement setup for GDRE Tools installation

--- src/main/java/dev/floraf/Mashup.java
+++ src/main/java/dev/floraf/Mashup.java
@@ -1,6 +1,8 @@
 package dev.floraf;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 
 public class Mashup {
@@ -11,5 +13,60 @@ public class Mashup {
 
     public static void main(String[] args) {
         LOGGER.info("Initializing Mashup, version {}...", VERSION);
+    }
+
+    public enum Platform {
+        WINDOWS("Windows"),
+        MACOS("macOS"),
+        LINUX("Linux");
+
+        public String name;
+
+        private Platform(String name) {
+            this.name = name;
+        }
+    }
+
+    public static Platform getPlatform() {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            return Platform.WINDOWS;
+        } else if (SystemUtils.IS_OS_MAC_OSX) {
+            return Platform.MACOS;
+        } else if (
+            SystemUtils.IS_OS_LINUX
+            || SystemUtils.IS_OS_FREE_BSD
+            || SystemUtils.IS_OS_OPEN_BSD
+            || SystemUtils.IS_OS_NET_BSD
+        ) {
+            return Platform.LINUX;
+        } else {
+            throw new UnsupportedOperationException("This OS is not supported!");
+        }
+    }
+
+    public static String getConfigDir() {
+        return switch (getPlatform()) {
+            case Platform.WINDOWS -> System.getenv("APPDATA")
+                + "/Mashup/config";
+            case Platform.MACOS -> System.getProperty("user.home")
+                + "/Library/Preferences/Mashup";
+            case Platform.LINUX -> ObjectUtils.firstNonNull(
+                System.getenv("XDG_CONFIG_HOME"),
+                System.getProperty("user.home") + "/.config"
+            ) + "/Mashup";
+        };
+    }
+
+    public static String getDataDir() {
+        return switch (getPlatform()) {
+            case Platform.WINDOWS -> System.getenv("APPDATA")
+                + "/Mashup/data";
+            case Platform.MACOS -> System.getProperty("user.home")
+                + "/Library/Application Support/Mashup";
+            case Platform.LINUX -> ObjectUtils.firstNonNull(
+                System.getenv("XDG_DATA_HOME"),
+                System.getProperty("user.home") + "/.local/share"
+            ) + "/Mashup";
+        };
     }
 }
--- src/main/java/dev/floraf/loader/external/GDREToolsInstall.java
+++ src/main/java/dev/floraf/loader/external/GDREToolsInstall.java
@@ -1,4 +1,82 @@
 package dev.floraf.loader.external;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import net.lingala.zip4j.ZipFile;
+import org.apache.commons.io.FileUtils;
+
+import dev.floraf.Mashup;
+
+// TODO: Wrap over GDRE Tools instance once finally installed.
+// TODO: Switch implementation of unzip to Apache Commons Compress to avoid
+//       having two different archive/compression libraries installed at the
+//       same time!
+
 public class GDREToolsInstall {
+    String gdreDir = Mashup.getDataDir() + "/gdre";
+    String version = "2.0.0";
+    ProcessBuilder gdre = new ProcessBuilder();
+
+    public GDREToolsInstall() {
+        if (!this.exists()) {
+            this.download();
+            this.install();
+        }
+    }
+
+    public boolean exists() {
+        Path gdrePath;
+        String gdreFile = switch (Mashup.getPlatform()) {
+            case Mashup.Platform.WINDOWS -> "gdre_tools.exe";
+            case Mashup.Platform.MACOS   -> "/Godot RE Tools.app/Contents/MacOS/Godot RE Tools";
+            case Mashup.Platform.LINUX   -> "gdre_tools.x86_64";
+        };
+
+        try {
+            gdrePath = Path.of(gdreDir + gdreFile);
+        } catch (InvalidPathException _) {
+            return false;
+        }
+
+        return Files.exists(gdrePath);
+    }
+
+    public void download() {
+        String os = Mashup.getPlatform().name.toLowerCase();
+
+        List<String> gdreURL = Arrays.asList (
+            "https://github.com/GDRETools/gdsdecomp/releases/download/v",
+            version, "/GDRE_tools-v", version, "-", os, ".zip"
+        );
+
+        try {
+            URL url = new URI(String.join("", gdreURL)).toURL();
+            FileUtils.copyURLToFile(
+                url, new File(gdreDir + "gdre.zip"), 10000, 5000);
+        } catch (MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException("Could not locate GDRE Tools!");
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to download GDRE Tools!");
+        }
+    }
+
+    public void install() {
+        String file = gdreDir + "gdre.zip";
+        try (var zip = new ZipFile(file)) {
+            zip.extractAll(gdreDir);
+            FileUtils.delete(new File(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
