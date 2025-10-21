bc299de16b73aec917455557a9f0964950d20388
Flora Fischbacher <227881669+FloraFischbacher@users.noreply.github.com>
2025-10-20 21:54:32 -0500

    Sketch new planned dependency type

--- src/main/java/dev/floraf/loader/data/DependencyType.java
+++ src/main/java/dev/floraf/loader/data/DependencyType.java
@@ -6,10 +6,10 @@ package dev.floraf.loader.data;
  */
 public enum DependencyType {
     /**
-     * Also called a "soft dependency". Mashup will attempt to load this patch,
-     * regardless of whether the dependency specified is present. However, if
-     * the resource this patch depends on is present, it will be loaded after
-     * the specified patch.
+     * Also called a "load-order dependency". Mashup will attempt to load this
+     * patch, regardless of whether the dependency specified is present.
+     * However, if the resource this patch depends on is present, it will be
+     * loaded after the specified patch.
      * 
      * Note that this dependency type does not allow game version or DLC
      * checks (unlike the other two types), as the game's DLC are patched in
@@ -19,17 +19,25 @@ public enum DependencyType {
      * the ordering of mods loaded may impact the targets of the mods' patches.
      */
     AWAIT,
+
     /**
-     * Also called a "firm dependency". Mashup will not load this patch if the
-     * resource it depends on is not present. It is important to note that
-     * if another patch REQUIREs (see below) a patch that has a firm
+     * Also called a "patch-level dependency". Mashup will not load this patch
+     * if the resource it depends on is not present. It is important to note
+     * that if another patch REQUIREs (see below) a patch that has a firm
      * dependency, unsatisfied dependencies could cascade into a mod-loader
      * halt.
      * 
      * Use of this dependency type is often for implementing integrations into
      * other mods or DLC that are optional in nature.
      */
+    REQUEST,
+
+    /**
+     * Also called a "mod-level dependency". Mashup will not load the mod this
+     * patch is associated with it the resource it depends on is not present.
+     */
     EXPECT,
+
     /**
      * Also called a "hard dependency". Mashup will halt the mod-loading
      * process if the resource that it depends on is not present.
@@ -38,5 +46,5 @@ public enum DependencyType {
      * Only use this dependency type if the _entire mod_ no longer makes sense
      * being present if the dependency isn't either.
      */
-    REQUIRE,
+    DEMAND,
 }
